409c687ebd0f5c1dd56f256e0a1955d3bc49f1b6
fachri-amin <fachri.amin@gmail.com>
2020-09-08 14:07:19 +0700

    add total users in admin page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,7 @@ use App\Category;
 use App\Comment;
 use App\Post;
 use App\Rating;
+use App\User;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -15,8 +16,8 @@ class AdminController extends Controller
         $all_categories = Category::all()->count();
         $all_comments = Comment::all()->count();
         $all_ratings = Rating::all()->count();
+        $all_users = User::all()->count();
 
-
-        return view('admin.index', compact('all_posts', 'all_categories', 'all_comments', 'all_ratings'));
+        return view('admin.index', compact('all_users', 'all_posts', 'all_categories', 'all_comments', 'all_ratings'));
     }
 }
